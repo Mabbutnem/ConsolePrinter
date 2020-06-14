5f99a102af56365eabec99f486d2337fb9e20974
Mabbutnem <thibault.harel1@gmail.com>
2020-06-14 17:41:13 +0200

    first commit

---
+++ ConsolePrinter/ConsolePrinter.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace ConsolePrinter
+{
+   class ConsolePrinter
+   {
+      private static readonly int NB_MIN_SPACE = 1;
+
+      public static void Print(string[] fields, string[][] values)
+      {
+         //Verification
+         foreach (string[] strArray in values)
+         {
+            if (strArray.Length != fields.Length)
+            {
+               Console.WriteLine("Can't print : fields and values don't have same Length.");
+               return;
+            }
+         }
+
+         //Calculate needed spaces
+         int[] maxStrLength = fields.Select(str => str.Length).ToArray();
+         foreach (string[] strArray in values)
+         {
+            for (int i = 0; i < strArray.Length; i++)
+            {
+               maxStrLength[i] = Math.Max(maxStrLength[i], strArray[i].Length);
+            }
+         }
+         maxStrLength = maxStrLength.Select(i => i + NB_MIN_SPACE).ToArray();
+
+         //Printing
+         Console.ForegroundColor = ConsoleColor.White;
+         for (int i = 0; i < fields.Length; i++)
+         {
+            Console.Write(fields[i]);
+            PrintSpace(maxStrLength[i] - fields[i].Length);
+         }
+         Console.ResetColor();
+         Console.WriteLine();
+         foreach (string[] strArray in values)
+         {
+            for (int i = 0; i < strArray.Length; i++)
+            {
+               Console.Write(strArray[i]);
+               PrintSpace(maxStrLength[i] - strArray[i].Length);
+            }
+            Console.WriteLine();
+         }
+      }
+
+      private static void PrintSpace(int nb)
+      {
+         for (int i = 0; i < nb; i++)
+         {
+            Console.Write(' ');
+         }
+      }
+   }
+}
---
+++ ConsolePrinter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsolePrinter
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         ConsolePrinter.Print(
+            new string[] { "Nom", "Age", "Ville", "Loisirs" },
+            new string[][]
+            {
+               new string[] {"Thibault", "22", "Laval",     "Jeux"},
+               new string[] {"Anthony",  "22", "Cholet",    "Netflix"},
+               new string[] {"Kevin",    "22", "Marseille", "Compter les chevaux"},
+               new string[] {"Patty",    "22", "Laval",     "Travail"},
+            }
+         );
+      }
+   }
+}
