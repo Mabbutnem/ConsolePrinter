2de74435d396d8a219c72b134ec8e5ce81d3651a
Mabbutnem <thibault.harel1@gmail.com>
2020-06-14 20:10:16 +0200

    color for console

--- ConsolePrinter/ConsolePrinter.cs
+++ ConsolePrinter/ConsolePrinter.cs
@@ -1,18 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
-using System.Text;
 
 namespace ConsolePrinter
 {
+   #region Console Printer
    class ConsolePrinter
    {
       private static readonly int NB_MIN_SPACE = 1;
 
       public static void Print(string[] fields, string[][] values)
       {
-         //Verification
+         //Checking length
          foreach (string[] strArray in values)
          {
             if (strArray.Length != fields.Length)
@@ -22,7 +20,7 @@ namespace ConsolePrinter
             }
          }
 
-         //Calculate needed spaces
+         //Calculating needed spaces
          int[] maxStrLength = fields.Select(str => str.Length).ToArray();
          foreach (string[] strArray in values)
          {
@@ -53,6 +51,61 @@ namespace ConsolePrinter
          }
       }
 
+      public static void Print(string[] fields, string[][] values, PrintingColor[] printingColors)
+      {
+         //Checking length
+         if (printingColors.Length != fields.Length)
+         {
+            Console.WriteLine("Can't print : fields and printing colors don't have same Length.");
+            return;
+         }
+         foreach (string[] strArray in values)
+         {
+            if (strArray.Length != fields.Length)
+            {
+               Console.WriteLine("Can't print : fields and values don't have same Length.");
+               return;
+            }
+         }
+
+         //Calculating needed spaces
+         int[] maxStrLength = fields.Select(str => str.Length).ToArray();
+         foreach (string[] strArray in values)
+         {
+            for (int i = 0; i < strArray.Length; i++)
+            {
+               maxStrLength[i] = Math.Max(maxStrLength[i], strArray[i].Length);
+            }
+         }
+         maxStrLength = maxStrLength.Select(i => i + NB_MIN_SPACE).ToArray();
+
+         //Printing
+         Console.ForegroundColor = ConsoleColor.White;
+         for (int i = 0; i < fields.Length; i++)
+         {
+            Console.Write(fields[i]);
+            PrintSpace(maxStrLength[i] - fields[i].Length);
+         }
+         Console.ResetColor();
+         Console.WriteLine();
+         foreach (string[] strArray in values)
+         {
+            for (int i = 0; i < strArray.Length; i++)
+            {
+               PrintingColor printingColor = printingColors[i];
+               if (printingColor.PrintingColorPredicate(strArray[i]))
+               {
+                  Console.ForegroundColor = printingColor.ForegroundColor;
+                  Console.BackgroundColor = printingColor.BackgroundColor;
+               }
+               Console.Write(strArray[i]);
+               PrintSpace(maxStrLength[i] - strArray[i].Length);
+               Console.ResetColor();
+            }
+            Console.WriteLine();
+         }
+      }
+
       private static void PrintSpace(int nb)
       {
          for (int i = 0; i < nb; i++)
@@ -61,4 +114,41 @@ namespace ConsolePrinter
          }
       }
    }
+   #endregion
+
+   #region Printing Color
+   class PrintingColor
+   {
+      public ConsoleColor ForegroundColor { get; }
+      public ConsoleColor BackgroundColor { get; }
+      public Predicate<string> PrintingColorPredicate { get; }
+
+      private PrintingColor(ConsoleColor foregroundColor, ConsoleColor backgroundColor, Predicate<string> printingColorPredicate)
+      {
+         ForegroundColor = foregroundColor;
+         BackgroundColor = backgroundColor;
+         PrintingColorPredicate = printingColorPredicate;
+      }
+
+      public static PrintingColor Create(ConsoleColor foregroundColor, ConsoleColor backgroundColor, Predicate<string> printingColorPredicate)
+      {
+         return new PrintingColor(foregroundColor, backgroundColor, printingColorPredicate);
+      }
+
+      public static PrintingColor Create(ConsoleColor foregroundColor, Predicate<string> printingColorPredicate)
+      {
+         return new PrintingColor(foregroundColor, ConsoleColor.Black, printingColorPredicate);
+      }
+
+      public static PrintingColor Base()
+      {
+         return new PrintingColor(ConsoleColor.Black, ConsoleColor.Black, (str) => false);
+      }
+
+      public static PrintingColor Invisible(Predicate<string> printingColorPredicate)
+      {
+         return new PrintingColor(ConsoleColor.Black, ConsoleColor.Black, printingColorPredicate);
+      }
+   }
+   #endregion
 }
--- ConsolePrinter/Program.cs
+++ ConsolePrinter/Program.cs
@@ -13,9 +13,27 @@ namespace ConsolePrinter
                new string[] {"Thibault", "22", "Laval",     "Jeux"},
                new string[] {"Anthony",  "22", "Cholet",    "Netflix"},
                new string[] {"Kevin",    "22", "Marseille", "Compter les chevaux"},
-               new string[] {"Patty",    "22", "Laval",     "Travail"},
+               new string[] {"Patty",    "23", "Laval",     "Travail"},
+            },
+            new PrintingColor[]
+            {
+               PrintingColor.Base(),
+               PrintingColor.Create(ConsoleColor.Red, EqualTo23),
+               PrintingColor.Create(ConsoleColor.Yellow, IsLaval),
+               PrintingColor.Base(),
             }
          );
+      }
+
+      static bool EqualTo23(string str)
+      {
+         bool parsable = int.TryParse(str, out int i);
+         return parsable && i == 23;
+      }
+
+      static bool IsLaval(string str)
+      {
+         return str.CompareTo("Laval")==0;
       }
    }
 }
